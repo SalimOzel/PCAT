a4b1347a018dccb43370e8e8b10b8756285c07e5
Salim Ozel <salimozel123@gmail.com>
2024-08-24 19:10:40 +0300

    db transaction

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require(`express`);
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const { urlencoded } = require('express');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{photos});
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -22,12 +27,13 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
-app.post('/photos', (req, res) => {
-  console.log(req.body);
-  res.redirect('/')
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
+ 
--- models/Photo.js
+++ models/Photo.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//connect DB
-mongoose.connect('mongodb://localhost/pct-test-db');
-
 //create schema
 const PhotoSchema = new Schema({
   title: String,
   description: String,
+  image: String,
+  dateCreated: { type: Date, default: Date.now },
 });
 
 const Photo = mongoose.model('Photo', PhotoSchema);
 
-//create a photo
-Photo.create({
-  title: 'Photo title 1',
-  description: 'Photo description 1 lorem ipsum ',
-});
+module.exports = Photo;
